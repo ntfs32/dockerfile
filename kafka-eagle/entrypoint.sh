b1bb321e3ba47c9597ec5bde510e7f2fb29bb7f3
ntfs32 <huhsuang123a@gmail.com>
2020-12-13 10:14:42 +0800

    update

--- kafka-eagle/entrypoint.sh
+++ kafka-eagle/entrypoint.sh
@@ -12,4 +12,4 @@ sed -i "s/PASSWORD/${PASSWORD}/g" /kafka-eagle/conf/system-config.properties
 chmod +x /kafka-eagle/bin/ke.sh
 mkdir -p /kafka-eagle/db/
 sh /kafka-eagle/bin/ke.sh start
-tail -f /kafka-eagle/logs/log
+tail -f /kafka-eagle/logs/log.log
